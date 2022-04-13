c29cb3d8a13db0cb8ff4300422d3a6d54ba29877
nqolak <39696574+nqolak@users.noreply.github.com>
2022-04-14 01:09:15 +0300

    Added about page.

--- EbookVault/Controllers/HomeController.cs
+++ EbookVault/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace EbookVault.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
