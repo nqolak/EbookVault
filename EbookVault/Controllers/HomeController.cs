aa30fcafb197c7049e48fb415a7d23a789078fea
nqolak <39696574+nqolak@users.noreply.github.com>
2022-04-14 14:34:32 +0300

    Added contacts page.

--- EbookVault/Controllers/HomeController.cs
+++ EbookVault/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace EbookVault.Controllers
             return View();
         }
 
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
