3ccd86ae2c587fcb1d5b092555dac69c40745f34
nqolak <39696574+nqolak@users.noreply.github.com>
2022-04-18 16:41:58 +0300

    Added User data model that extends IdentityUser.

---
+++ EbookVault.Infrastructure/Data/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EbookVault.Infrastructure.Data
+{
+    public class User : IdentityUser
+    {
+        public bool IsDeleted { get; set; } = false;
+
+        // TODO
+
+        public HashSet<Book> UserBooks { get; set; } = new HashSet<Book>();
+    }
+}
