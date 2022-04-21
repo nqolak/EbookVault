552119e1fc068a5d0be896f34af85b89c040e121
nqolak <39696574+nqolak@users.noreply.github.com>
2022-04-21 21:46:02 +0300

    Renamed tables and fields to be more accurate.

---
+++ EbookVault.Infrastructure/Migrations/20220421181532_RenamedTableBookUser.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EbookVault.Infrastructure.Migrations
+{
+    public partial class RenamedTableBookUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "BookUser", 
+                newName: "UsersBooks");
+
+            migrationBuilder.RenameColumn(
+                name: "BooksId",
+                table: "UsersBooks",
+                newName: "BookId");
+
+            migrationBuilder.RenameColumn(
+                name: "UsersId",
+                table: "UsersBooks",
+                newName: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "UsersBooks",
+                newName: "BookUser");
+
+            migrationBuilder.RenameColumn(
+                name: "BookId",
+                table: "BookUser",
+                newName: "BooksId");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "BookUser",
+                newName: "UsersId");
+        }
+    }
+}
---
+++ EbookVault.Infrastructure/Migrations/20220421182753_RenamedTableBookGenre.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EbookVault.Infrastructure.Migrations
+{
+    public partial class RenamedTableBookGenre : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "BookGenre",
+                newName: "BooksGenres");
+
+            migrationBuilder.RenameColumn(
+                name: "BooksId",
+                table: "BooksGenres",
+                newName: "BookId");
+
+            migrationBuilder.RenameColumn(
+                name: "GenresId",
+                table: "BooksGenres",
+                newName: "GenreId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "BooksGenres",
+                newName: "BookGenre");
+
+            migrationBuilder.RenameColumn(
+                name: "BookId",
+                table: "BookGenre",
+                newName: "BooksId");
+
+            migrationBuilder.RenameColumn(
+                name: "GenreId",
+                table: "BookGenre",
+                newName: "GenresId");
+        }
+    }
+}
